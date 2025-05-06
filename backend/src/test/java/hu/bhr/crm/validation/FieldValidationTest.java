c58c11f694bf9eada71d8903a5a7bd0d011e4f47
Adam Kosa <adykaa.coding@gmail.com>
2025-05-06 18:29:14 +0200

    make test case names clearer

--- backend/src/test/java/hu/bhr/crm/validation/FieldValidationTest.java
+++ backend/src/test/java/hu/bhr/crm/validation/FieldValidationTest.java
@@ -8,48 +8,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class FieldValidationTest {
 
     @Test
-    void testValidField() {
+    void shouldSuccessWhenFieldPresent() {
         assertDoesNotThrow(() -> FieldValidation.validateNotEmpty("John Doe", "name"));
     }
 
     @Test
-    void testEmptyField() {
+    void shouldFailWhenFieldEmpty() {
         assertThrows(MissingFieldException.class, () -> FieldValidation.validateNotEmpty("", "name"));
     }
 
     @Test
-    void testNullField() {
+    void shouldFailWhenFieldNull() {
         assertThrows(MissingFieldException.class, () -> FieldValidation.validateNotEmpty(null, "name"));
     }
 
     @Test
-    void testWhitespaceField() {
+    void shouldFailWhenFieldWhitespace() {
         assertThrows(MissingFieldException.class, () -> FieldValidation.validateNotEmpty("   ", "name"));
     }
 
 
     @Test
-    void testFirstFieldPresentSecondEmpty() {
+    void shouldSuccessWhenFirstFieldPresentSecondEmpty() {
         assertDoesNotThrow(() -> FieldValidation.validateAtLeastOneIsNotEmpty("John", "First Name", "", "Nickname"));
     }
 
     @Test
-    void testSecondFieldPresentFirstNull() {
+    void shouldSuccessWhenFirstFieldNullSecondPresent() {
         assertDoesNotThrow(() -> FieldValidation.validateAtLeastOneIsNotEmpty(null, "First Name", "Johnny", "Nickname"));
     }
 
     @Test
-    void testBothFieldsPresent() {
+    void shouldSuccessWhenBothFieldsPresent() {
         assertDoesNotThrow(() -> FieldValidation.validateAtLeastOneIsNotEmpty("John", "First Name", "Johnny", "Nickname"));
     }
 
     @Test
-    void testBothFieldsEmpty() {
+    void shouldFailWhenBothFieldsEmpty() {
         assertThrows(MissingFieldException.class, () -> FieldValidation.validateAtLeastOneIsNotEmpty("", "First Name", "", "Nickname"));
     }
 
     @Test
-    void testBothFieldsNull() {
+    void shouldFailWhenBothFieldsNull() {
         assertThrows(MissingFieldException.class, () -> FieldValidation.validateAtLeastOneIsNotEmpty(null, "First Name", null, "Nickname"));
     }
 }
